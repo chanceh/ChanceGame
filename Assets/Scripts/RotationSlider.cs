266a6039d097ff82aa569643381126250322d700
Heath <chance.heath@perkinswill.com>
2019-05-13 18:08:54 -0500

    added sliders and slider script

---
+++ Assets/Scripts/RotationSlider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotationSlider : MonoBehaviour
+{
+    public Slider[] sliders;
+
+    public void Start()
+    {
+        for (int i = 0; i < sliders.Length+ -1; i++)
+        {
+            Slider sldrs = sliders[i].GetComponent<Slider>();
+            sldrs.onValueChanged.AddListener(delegate { ValueChangeCheck(i); });
+        }
+    }
+
+    public void ValueChangeCheck(int sliderIndex)
+    {
+        Debug.Log("Slider #" + sliderIndex, sliders[sliderIndex]);
+    }
+}
